8dc37f5c3d793ae2648d64faaf90959a92f704b7
Diana Badas <diana.badas@gmail.com>
2019-08-09 10:37:45 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,11 @@
 class RepositoriesController < ApplicationController
   def index
-    github = GithubService.new
-    github.get_repos
+    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    @repos_array = JSON.parse(response.body)
   end
 
   def create
-    github = GithubService.new
-    github.create_repo(params[:name])
+    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    redirect_to '/'
   end
 end
